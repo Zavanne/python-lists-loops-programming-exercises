73f37bf0e19e09eba8309efb8c1766a5e40859cb
Mariano Marcos <140490758+Zavanne@users.noreply.github.com>
2024-07-19 18:08:54 +0000

    Exercises finished

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -22,3 +22,23 @@ print(lyrics_generator([0,0,1,1,1,0,0,0]))
 print(lyrics_generator([0,0,0]))
 print(lyrics_generator([1,0,1]))
 print(lyrics_generator([1,1,1]))
+
+# def lyrics_generator(list_of_beats):
+#     song_lyrics = []
+#     effect = 0
+    
+#     for beat in list_of_beats:
+#         if beat == 0:
+#             song_lyrics.append("Boom")
+#             effect = 0
+#         else:
+#             song_lyrics.append("Drop the bass")
+#             effect += 1
+        
+#         if effect == 3:
+#             song_lyrics.append("!!!Break the bass!!!")
+#             effect = 0
+    
+#     return " ".join(song_lyrics) + " "
+
+
